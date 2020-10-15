2123e57016dcbda8f8f7a0a3d40631ca7f7eee2a
Didier Roche <didrocks@ubuntu.com>
2020-10-15 09:49:38 +0200

    Fix import by previous package relocation

--- internal/policies/ad/registry/registry_test.go
+++ internal/policies/ad/registry/registry_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/ubuntu/adsys/internal/registry"
+	"github.com/ubuntu/adsys/internal/policies/ad/registry"
 )
 
 func TestDecodePolicy(t *testing.T) {
