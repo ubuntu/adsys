5adbcfe346377ceefed685b83a87ce260e7a1378
Didier Roche <didrocks@ubuntu.com>
2020-10-15 18:34:15 +0200

    Viper configuration helpers
    Abstract configuration by env variables and configuration files into the
    config package.
    A Configure helper and a DefaultLoadConfig illustrate how we can hook up
    this configuration loading.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,13 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // TEXTDOMAIN is the gettext domain for l10n
@@ -20,4 +26,68 @@ func SetVerboseMode(level int) {
 	case 2:
 		log.SetLevel(log.DebugLevel)
 	}
+}
+
+// Configure sets verbosity level and add config env variables and file support based on name prefix.
+// It call the refreshConfig function so that you can deserialized the configuration and returns any error.
+// It automatically watches any configuration changes and will call refreshConfig.
+func Configure(name string, rootCmd cobra.Command, refreshConfig func() error) (err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("couldn't load configuration: %v", err)
+		}
+	}()
+
+	// Get cmdline flag for verbosity to configure logger until we have everything parsed.
+	v, err := rootCmd.PersistentFlags().GetCount("verbose")
+	fmt.Println(err)
+	if err != nil {
+		return fmt.Errorf("internal error: no persistent verbose flag installed on rootCmd: %v", err)
+	}
+
+	SetVerboseMode(v)
+
+	viper.SetConfigName(name)
+	viper.AddConfigPath("./")
+	viper.AddConfigPath("$HOME/")
+	viper.AddConfigPath("/etc/")
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Infof("Config file %s changed. Reloading.", e.Name)
+		if err := refreshConfig(); err != nil {
+			log.Warningf("Error while refreshing configuration: %v", err)
+		}
+	})
+	if err := viper.ReadInConfig(); err != nil {
+		var e viper.ConfigFileNotFoundError
+		if errors.As(err, &e) {
+			log.Infof("No configuration file: %v.\nWe will use the default.", e)
+		} else {
+			return fmt.Errorf("invalid configuration file: %v", err)
+		}
+	}
+
+	viper.SetEnvPrefix(name)
+	viper.AutomaticEnv()
+
+	if err := refreshConfig(); err != nil {
+		return fmt.Errorf("error while refreshing configuration: %v", err)
+	}
+
+	return nil
+}
+
+type verboser interface {
+	Verbosity() int
+}
+
+// DefaultLoadConfig takes c and unmarshall the config to it. It also switch verbositty is c has a Verbosity() method
+func DefaultLoadConfig(c interface{}) error {
+	if err := viper.Unmarshal(&c); err != nil {
+		return fmt.Errorf("unable to decode configuration into struct: %v", err)
+	}
+	if c, ok := c.(verboser); ok {
+		SetVerboseMode(c.Verbosity())
+	}
+	return nil
 }
