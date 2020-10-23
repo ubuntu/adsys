4eb2fca44e6e22456fba5aa8fa39453c02a06379
Didier Roche <didrocks@ubuntu.com>
2020-10-23 12:12:09 +0200

    Server-side stream interceptor
    The server receives the metadata from client, telling which client ID
    and caller arguments are wanted.
    It will assign a new request ID for each request and wire up connection
    streaming to the client.
    Some options like which local logger to use and if we should caller are
    passed as parameters when initializing the interceptor.

---
+++ internal/grpc/logstreamer/server.go
@@ -0,0 +1,139 @@
+package logstreamer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/ubuntu/adsys/internal/i18n"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var logContextKey = struct{}{}
+
+type logContext struct {
+	idRequest           string
+	sendStream          sendStreamFn
+	withCallerForRemote bool
+	localLogger         *logrus.Logger
+	withCallerForLocal  bool
+}
+
+// StreamServerInterceptor wrap the server stream to create a new dedicated logger to stream back the logs.
+// It will use serverLogger to log locally the same messages, prefixing by the request ID.
+// reportCaller allows to report the real caller name. This function will call SetReportCaller(false) on the logger then.
+func StreamServerInterceptor(localLogger *logrus.Logger, reportCaller bool) func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	// We are controlling and unwrapping the caller ourself outside of this package. As logrus doesn't allow to specify
+	// which package to exclude manually, do it there.
+	localLogger.SetReportCaller(false)
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		clientID, withCaller, err := extractMetaFromContext(ss.Context())
+		if err != nil {
+			return err
+		}
+
+		ssLogs := serverStreamWithLogs{
+			ServerStream: ss,
+		}
+
+		// create and log request ID
+		idRequest := fmt.Sprintf("%s:%s", clientID, createID())
+		if err := ssLogs.sendLogs(logrus.DebugLevel.String(), fmt.Sprintf(i18n.G("Connecting as [[%s]]"), idRequest)); err != nil {
+			localLogger.Warningf(localLogFormatWithID, i18n.G("couldn't send initial connection log to client"))
+		}
+		localLogger.Infof(i18n.G("New connection from client [[%s]]"), idRequest)
+
+		// attach stream logger options to context so that we can log locally and remotely from context
+		ssLogs.ctx = context.WithValue(ss.Context(), logContextKey, logContext{
+			idRequest:           idRequest,
+			sendStream:          ssLogs.sendLogs,
+			withCallerForRemote: withCaller,
+			localLogger:         localLogger,
+			withCallerForLocal:  reportCaller,
+		})
+
+		return handler(srv, ssLogs)
+	}
+}
+
+type serverStreamWithLogs struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+func (ss serverStreamWithLogs) Context() context.Context {
+	return ss.ctx
+}
+
+// sendLogs sends directly to the stream a Log message with dedicated entries.
+// This will be intercepted by the StreamClientInterceptor for every Log message matching
+// its structure, preventing to hit the client.
+// A harcoded header is set to double check and ensure we have Log message.
+func (ss serverStreamWithLogs) sendLogs(logLevel, msg string) error {
+	return ss.SendMsg(&Log{
+		LogHeader: logIdentifier,
+		Level:     logLevel,
+		Msg:       msg,
+	})
+}
+
+type sendStreamFn func(logLevel, msg string) error
+
+func extractMetaFromContext(ctx context.Context) (clientID string, withCaller bool, err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf(i18n.G("Invalid metdata from client: %v\n. Please use the StreamClientInterceptor."), err)
+		}
+	}()
+
+	// extract logs metadata from the client
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", false, errors.New(i18n.G("missing client metadata"))
+	}
+	clientID, err = validUniqueMdEntry(md, clientIDKey)
+	if err != nil {
+		return "", false, err
+	}
+	withCallerRaw, err := validUniqueMdEntry(md, clientWantCallerKey)
+	if err != nil {
+		return "", false, err
+	}
+	withCaller, err = strconv.ParseBool(withCallerRaw)
+	if err != nil {
+		return "", false, fmt.Errorf(i18n.G("%s isn't a boolean: %v"), clientWantCallerKey, err)
+	}
+
+	return clientID, withCaller, nil
+}
+
+func validUniqueMdEntry(md metadata.MD, key string) (string, error) {
+	v := md.Get(key)
+	if len(v) == 0 {
+		return "", fmt.Errorf(i18n.G("missing metadata %s for incoming request"), key)
+	}
+	if len(v) != 1 {
+		return "", fmt.Errorf(i18n.G("invalid metadata %s for incoming request: %q"), key, v)
+	}
+	return v[0], nil
+}
+
+var seedOnce = sync.Once{}
+
+func createID() (id string) {
+	seedOnce.Do(func() {
+		rand.Seed(time.Now().Unix())
+	})
+
+	id = strconv.Itoa(rand.Int())
+	runes := []rune(id)
+	id = string(runes[0:6])
+
+	return id
+}
