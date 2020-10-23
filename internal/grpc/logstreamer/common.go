0b1416babb3e320fa95c126559b51b95979e70bb
Didier Roche <didrocks@ubuntu.com>
2020-10-23 12:07:00 +0200

    Add common definitions files and proto generator for logstreamer

---
+++ internal/grpc/logstreamer/common.go
@@ -0,0 +1,10 @@
+package logstreamer
+
+//go:generate protoc --go_out=. --go_opt=paths=source_relative log.proto
+
+const (
+	logIdentifier = "LOGSTREAMER_MSG"
+
+	clientIDKey         = "ClientID"
+	clientWantCallerKey = "ClientWantCallery"
+)
