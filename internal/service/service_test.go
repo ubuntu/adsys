a69039e5bda9810705f554eb35b1fe2b866d80dc
Didier Roche <didrocks@ubuntu.com>
2020-10-28 08:35:49 +0100

    Systemd activation and notification tests

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,6 +1,8 @@
 package service_test
 
 import (
+	"errors"
+	"net"
 	"path/filepath"
 	"testing"
 	"time"
@@ -56,6 +58,117 @@ func TestServerChangeSocket(t *testing.T) {
 
 	err = s.Listen()
 	require.NoError(t, err, "Listen should return no error when stopped after changing socket")
+}
+
+func TestServerSocketActivation(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		sockets      []string
+		listenerFail bool
+
+		wantErr bool
+	}{
+		"success with one socket": {sockets: []string{"sock1"}},
+
+		"fails when Listeners() fails": {listenerFail: true, wantErr: true},
+		"fails with many sockets":      {sockets: []string{"socket1", "socket2"}, wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			dir := t.TempDir()
+
+			var listeners []net.Listener
+			for _, socket := range tc.sockets {
+				l, err := net.Listen("unix", filepath.Join(dir, socket))
+				require.NoErrorf(t, err, "setup failed: couldn't create unix socket: %v", err)
+				defer l.Close()
+				listeners = append(listeners, l)
+			}
+
+			var f func() ([]net.Listener, error)
+			if tc.listenerFail {
+				f = func() ([]net.Listener, error) {
+					return nil, errors.New("systemd activation error")
+				}
+			} else {
+				f = func() ([]net.Listener, error) {
+					return listeners, nil
+				}
+			}
+
+			s, err := service.New("/tmp/this/is/ignored", service.WithSystemdActivationListener(f))
+			if tc.wantErr {
+				require.NotNil(t, err, "New should return an error")
+				return
+			} else if !tc.wantErr {
+				require.NoError(t, err, "New should return no error")
+			}
+
+			go func() {
+				time.Sleep(10 * time.Millisecond)
+				s.Quit()
+			}()
+			err = s.Listen()
+			require.NoError(t, err, "Listen should return no error")
+		})
+	}
+}
+
+func TestServerSdNotifier(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		sent         bool
+		notifierFail bool
+
+		wantErr bool
+	}{
+		"sends signal":                        {sent: true},
+		"doesn't fail when not under systemd": {sent: false},
+
+		"fails when notifier fails": {notifierFail: true, wantErr: true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			dir := t.TempDir()
+
+			l, err := net.Listen("unix", filepath.Join(dir, "socket"))
+			require.NoErrorf(t, err, "setup failed: couldn't create unix socket: %v", err)
+			defer l.Close()
+
+			s, err := service.New("/tmp/this/is/ignored",
+				service.WithSystemdActivationListener(func() ([]net.Listener, error) { return []net.Listener{l}, nil }),
+				service.WithSystemdSdNotifier(func(unsetEnvironment bool, state string) (bool, error) {
+					if tc.notifierFail {
+						return false, errors.New("systemd notifier error")
+					}
+					return tc.sent, nil
+				}))
+			require.NoError(t, err, "New should return no error")
+
+			go func() {
+				time.Sleep(10 * time.Millisecond)
+				s.Quit()
+			}()
+
+			err = s.Listen()
+			if tc.wantErr {
+				require.NotNil(t, err, "Listen should return an error")
+				return
+			} else if !tc.wantErr {
+				require.NoError(t, err, "Listen should return no error")
+			}
+		})
+	}
 }
 
 func TestServerFailingOption(t *testing.T) {
