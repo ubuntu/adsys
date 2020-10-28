8f48f8d043e8217d6950e61c817c1f8c7669fb5d
Didier Roche <didrocks@ubuntu.com>
2020-10-28 08:35:49 +0100

    Systemd activation and notification tests

--- internal/service/export_test.go
+++ internal/service/export_test.go
@@ -2,7 +2,22 @@ package service
 
 import (
 	"errors"
+	"net"
 )
+
+func WithSystemdActivationListener(f func() ([]net.Listener, error)) func(o *options) error {
+	return func(o *options) error {
+		o.systemdActivationListener = f
+		return nil
+	}
+}
+
+func WithSystemdSdNotifier(f func(unsetEnvironment bool, state string) (bool, error)) func(o *options) error {
+	return func(o *options) error {
+		o.systemdSdNotifier = f
+		return nil
+	}
+}
 
 func FailingOption() func(o *options) error {
 	return func(o *options) error {
