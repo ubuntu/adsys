c1ac52c298d57a6bdd99e85b51caad49c4c68982
Didier Roche <didrocks@ubuntu.com>
2020-10-29 12:42:13 +0100

    Split between daemon and service
    Abstract the service implementation to the daemon now, so that the
    daemon part can be reused accross projects.
    We inject the service via a GRPC registerer function that is passed to
    the constructor.

---
+++ internal/adsysservice/adsysservice.go
@@ -0,0 +1,27 @@
+package adsysservice
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/ubuntu/adsys"
+	"github.com/ubuntu/adsys/internal/daemon"
+	"github.com/ubuntu/adsys/internal/grpc/connectionnotify"
+	"github.com/ubuntu/adsys/internal/grpc/interceptorschainer"
+	log "github.com/ubuntu/adsys/internal/grpc/logstreamer"
+	"google.golang.org/grpc"
+)
+
+// Service is used to implement adsys.ServiceServer.
+type Service struct {
+	adsys.UnimplementedServiceServer
+}
+
+// RegisterGRPCServer registers our service with the new interceptor chains.
+// It will notify the daemon of any new connection
+func (s *Service) RegisterGRPCServer(d *daemon.Daemon) *grpc.Server {
+	srv := grpc.NewServer(grpc.StreamInterceptor(
+		interceptorschainer.ChainStreamServerInterceptors(
+			connectionnotify.StreamServerInterceptor(d),
+			log.StreamServerInterceptor(logrus.StandardLogger()))))
+	adsys.RegisterServiceServer(srv, s)
+	return srv
+}
