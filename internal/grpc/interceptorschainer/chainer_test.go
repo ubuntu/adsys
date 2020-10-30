6d56b1c5d5601944d1669b36cc9696ced2f25747
Didier Roche <didrocks@ubuntu.com>
2020-10-30 13:17:55 +0100

    Tests for ChainStreamServerInterceptors
    heavily inspired from go-grpc-middleware

---
+++ internal/grpc/interceptorschainer/chainer_test.go
@@ -0,0 +1,113 @@
+package interceptorschainer_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/ubuntu/adsys/internal/grpc/interceptorschainer"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestChainStreamServerInterceptors(t *testing.T) {
+	t.Parallel()
+
+	someService := &struct{}{}
+	someServiceName := "MyService"
+	recvMessage := "received"
+	sentMessage := "sent"
+	outputError := fmt.Errorf("some error")
+
+	parentContext := context.WithValue(context.TODO(), "parent", 42)
+	parentStreamInfo := &grpc.StreamServerInfo{
+		FullMethod:     someServiceName,
+		IsServerStream: true,
+	}
+
+	first := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		requireContextValue(t, 42, stream.Context(), "parent", "first interceptor must know the parent context value")
+		require.Equal(t, parentStreamInfo, info, "first interceptor must know the parentStreamInfo")
+		require.Equal(t, someService, srv, "first interceptor must know someService")
+		wrapped := wrapServerStream(stream)
+		wrapped.wrappedContext = context.WithValue(stream.Context(), "first", 43)
+		return handler(srv, wrapped)
+	}
+	second := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		requireContextValue(t, 42, stream.Context(), "parent", "second interceptor must know the parent context value")
+		requireContextValue(t, 43, stream.Context(), "first", "second interceptor must know the first context value")
+		require.Equal(t, parentStreamInfo, info, "second interceptor must know the parentStreamInfo")
+		require.Equal(t, someService, srv, "second interceptor must know someService")
+		wrapped := wrapServerStream(stream)
+		wrapped.wrappedContext = context.WithValue(stream.Context(), "second", 44)
+		return handler(srv, wrapped)
+	}
+	handler := func(srv interface{}, stream grpc.ServerStream) error {
+		require.Equal(t, someService, srv, "handler must know someService")
+		requireContextValue(t, 42, stream.Context(), "parent", "handler must know the parent context value")
+		requireContextValue(t, 43, stream.Context(), "first", "handler must know the first context value")
+		requireContextValue(t, 44, stream.Context(), "second", "handler must know the second context value")
+		require.NoError(t, stream.RecvMsg(recvMessage), "handler must have access to stream messages")
+		require.NoError(t, stream.SendMsg(sentMessage), "handler must be able to send stream messages")
+		return outputError
+	}
+	fakeStream := &fakeServerStream{ctx: parentContext, recvMessage: recvMessage}
+	chain := interceptorschainer.ChainStreamServerInterceptors(first, second)
+	err := chain(someService, fakeStream, parentStreamInfo, handler)
+	require.Equal(t, outputError, err, "chain must return handler's error")
+	require.Equal(t, sentMessage, fakeStream.sentMessage, "handler's sent message must propagate to stream")
+}
+
+func requireContextValue(t *testing.T, expected interface{}, ctx context.Context, key string, msg ...interface{}) {
+	val := ctx.Value(key)
+	require.NotNil(t, val, msg...)
+	require.Equal(t, expected, val, msg...)
+}
+
+// wrappedServerStream is a thin wrapper around grpc.ServerStream that allows modifying context.
+type wrappedServerStream struct {
+	grpc.ServerStream
+	// wrappedContext is the wrapper's own Context. You can assign it.
+	wrappedContext context.Context
+}
+
+// Context returns the wrapper's WrappedContext, overwriting the nested grpc.ServerStream.Context()
+func (w *wrappedServerStream) Context() context.Context {
+	return w.wrappedContext
+}
+
+// wrapServerStream returns a ServerStream that has the ability to overwrite context.
+func wrapServerStream(stream grpc.ServerStream) *wrappedServerStream {
+	if existing, ok := stream.(*wrappedServerStream); ok {
+		return existing
+	}
+	return &wrappedServerStream{ServerStream: stream, wrappedContext: stream.Context()}
+}
+
+type fakeServerStream struct {
+	grpc.ServerStream
+	ctx         context.Context
+	recvMessage interface{}
+	sentMessage interface{}
+}
+
+func (f *fakeServerStream) Context() context.Context {
+	return f.ctx
+}
+
+func (f *fakeServerStream) SendMsg(m interface{}) error {
+	if f.sentMessage != nil {
+		return status.Errorf(codes.AlreadyExists, "fakeServerStream only takes one message, sorry")
+	}
+	f.sentMessage = m
+	return nil
+}
+
+func (f *fakeServerStream) RecvMsg(m interface{}) error {
+	if f.recvMessage == nil {
+		return status.Errorf(codes.NotFound, "fakeServerStream has no message, sorry")
+	}
+	return nil
+}
