5613d72a1a22f05b23f87497cef5ee62e33cb196
Didier Roche <didrocks@ubuntu.com>
2020-11-06 09:45:16 +0100

    Implement version request
    Prints both version from client and service.

--- cmd/adsysd/client/version.go
+++ cmd/adsysd/client/version.go
@@ -2,8 +2,11 @@ package client
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/spf13/cobra"
+	"github.com/ubuntu/adsys"
+	"github.com/ubuntu/adsys/internal/adsysservice"
 	"github.com/ubuntu/adsys/internal/config"
 	"github.com/ubuntu/adsys/internal/i18n"
 )
@@ -13,13 +16,36 @@ func (a *App) installVersion() {
 		Use:   "version",
 		Short: i18n.G("Returns version of client and service"),
 		Args:  cobra.NoArgs,
-		RunE:  func(cmd *cobra.Command, args []string) error { return getVersion() },
+		RunE:  func(cmd *cobra.Command, args []string) error { return a.getVersion() },
 	}
 	a.rootCmd.AddCommand(cmd)
 }
 
 // getVersion returns the current server and client versions.
-func getVersion() (err error) {
+func (a App) getVersion() (err error) {
 	fmt.Printf(i18n.G("%s\t%s")+"\n", CmdName, config.Version)
+
+	client, err := adsysservice.NewClient(a.config.Socket, a.getTimeout())
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	stream, err := client.Version(a.ctx, &adsys.Empty{})
+	if err != nil {
+		return err
+	}
+
+	for {
+		version, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		fmt.Printf(i18n.G("%s\t\t%s")+"\n", "adsysd", version.GetVersion())
+	}
+
 	return nil
 }
