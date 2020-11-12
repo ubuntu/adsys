b0d148c6acf9b84b45754988da934a39618d29a9
Didier Roche <didrocks@ubuntu.com>
2020-11-12 17:05:52 +0100

    Foward caller to logger if requested
    Register and send caller if any of local, remote or forwarder asked for
    callstack information.
    In that case, the formatted message is only forwarded to clients who
    requested them.

--- internal/grpc/logstreamer/forwarders.go
+++ internal/grpc/logstreamer/forwarders.go
@@ -11,26 +11,51 @@ var (
 )
 
 type streamsForwarder struct {
-	fw map[grpc.Stream]bool
+	fw map[streamWithCaller]bool
 	mu sync.RWMutex
 
 	once sync.Once
+}
+
+type streamWithCaller struct {
+	grpc.Stream
+	wantsCaller bool
 }
 
 // AddStreamToForward adds stream identified to forward all logs to it.
 func AddStreamToForward(stream grpc.Stream) func() {
 	// Initialize our forwarder
 	streamsForwarders.once.Do(func() {
-		streamsForwarders.fw = make(map[grpc.Stream]bool)
+		streamsForwarders.fw = make(map[streamWithCaller]bool)
 	})
+
+	var wantsCaller bool
+	if logCtx, withLogCtx := stream.Context().Value(logContextKey).(logContext); withLogCtx {
+		wantsCaller = logCtx.withCallerForRemote
+	}
 
 	streamsForwarders.mu.Lock()
 	defer streamsForwarders.mu.Unlock()
-	streamsForwarders.fw[stream] = true
+	streamWcaller := streamWithCaller{
+		Stream:      stream,
+		wantsCaller: wantsCaller,
+	}
+	streamsForwarders.fw[streamWcaller] = true
 
 	return func() {
 		streamsForwarders.mu.Lock()
 		defer streamsForwarders.mu.Unlock()
-		delete(streamsForwarders.fw, stream)
+		delete(streamsForwarders.fw, streamWcaller)
 	}
+}
+
+func (sf *streamsForwarder) wantsCaller() bool {
+	sf.mu.RLock()
+	defer sf.mu.RUnlock()
+	for stream := range sf.fw {
+		if stream.wantsCaller {
+			return true
+		}
+	}
+	return false
 }
