75c12fe1bb94de6b50ee6a90f91e0d90f4cb2d3b
Didier Roche <didrocks@ubuntu.com>
2020-11-12 10:15:36 +0100

    CLI side for Cat command
    Available through adsysctl service cat.
    Client can control the log level.
    The command is blocking until we Ctrl+C.
    Multiple cat command can be ran in parallel, with different log levels.

--- cmd/adsysd/client/service.go
+++ cmd/adsysd/client/service.go
@@ -1,18 +1,58 @@
 package client
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/spf13/cobra"
+	"github.com/ubuntu/adsys"
+	"github.com/ubuntu/adsys/internal/adsysservice"
 	"github.com/ubuntu/adsys/internal/cmdhandler"
 	"github.com/ubuntu/adsys/internal/i18n"
 )
 
 func (a *App) installService() {
-	cmd := &cobra.Command{
+	mainCmd := &cobra.Command{
 		Use:   "service COMMAND",
 		Short: i18n.G("Service management"),
 		Args:  cmdhandler.SubcommandsRequiredWithSuggestions,
 		RunE:  cmdhandler.NoCmd,
 	}
+	a.rootCmd.AddCommand(mainCmd)
 
-	a.rootCmd.AddCommand(cmd)
+	cmd := &cobra.Command{
+		Use:   "cat",
+		Short: i18n.G("Print service logs"),
+		Args:  cobra.NoArgs,
+		RunE:  a.serviceCat,
+	}
+	mainCmd.AddCommand(cmd)
+
+}
+
+func (a *App) serviceCat(cmd *cobra.Command, args []string) error {
+	// No timeout for cat command
+	client, err := adsysservice.NewClient(a.config.Socket, 0)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	stream, err := client.Cat(a.ctx, &adsys.Empty{})
+	if err != nil {
+		return err
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		fmt.Print(msg.GetMsg())
+	}
+
+	return nil
 }
