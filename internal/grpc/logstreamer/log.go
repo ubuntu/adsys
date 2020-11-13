e92b29b647ddb3108fe3e16d521618b5f05ca535
Didier Roche <didrocks@ubuntu.com>
2020-11-13 08:20:22 +0100

    Cache showCaller to not loop over all forwarders everytime.

--- internal/grpc/logstreamer/log.go
+++ internal/grpc/logstreamer/log.go
@@ -135,13 +135,9 @@ func log(ctx context.Context, level logrus.Level, args ...interface{}) {
 	callerForLocal := localLogger.ReportCaller
 	callerMu.RUnlock()
 
-	streamsForwarders.mu.RLock()
-	callerForForwarders := streamsForwarders.showCaller()
-	streamsForwarders.mu.RUnlock()
-
 	// Handle call stack collect
 	var caller string
-	if callerForLocal || callerForRemote || callerForForwarders {
+	if callerForLocal || callerForRemote || streamsForwarders.showCaller {
 		f := getCaller()
 		fqfn := strings.Split(f.Function, "/")
 		fqfn = strings.Split(fqfn[len(fqfn)-1], ".")
