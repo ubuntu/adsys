72d51eb5f8a73714f8c74092058849eefdc7ca7a
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2020-11-25 11:39:06 +0100

    Added tests for GetPolicies.
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

--- internal/policies/ad/ad_test.go
+++ internal/policies/ad/ad_test.go
@@ -2,12 +2,18 @@ package ad_test
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/ubuntu/adsys/internal/policies/ad"
+	"github.com/ubuntu/adsys/internal/policies/policy"
 )
 
 func TestNew(t *testing.T) {
@@ -57,4 +63,383 @@ func TestNew(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGetPolicies(t *testing.T) {
+	//t.Parallel() // libsmbclient overrides SIGCHILD, keep one AD object
+
+	var krb5CCcontent = []byte("KRB5 Ticket file content")
+
+	hostname, err := os.Hostname()
+	require.NoError(t, err, "Setup: failed to get hostname")
+
+	/*
+		GPOs layout:
+
+		User
+			standard	A:standardA B:standardB C:standardC
+			user-only	A:userOnlyA B:userOnlyB
+			one-value	C:oneValueC
+			disabled-value C
+
+		Machine
+			standard		A:standardA D:standardD E:standardE
+			machine-only	A:machOnlyA D:machOnlyD
+			one-value		E:oneValueE
+			disabled-value C
+	*/
+
+	/*
+		TODO:
+		- Verify if Registry.pol always exists or not. If it doesn't when no value
+		  is modified for the host, it'll defeat the strategy to set default values.
+	*/
+	tests := map[string]struct {
+		gpoListArgs        string
+		objectName         string
+		objectClass        ad.ObjectClass
+		userKrb5CCBaseName string
+
+		dontCreateOriginalKrb5CCName bool
+		turnKrb5CCRO                 bool
+
+		want    map[string]policy.Entry
+		wantErr bool
+	}{
+		"Standard policy, user object": {
+			gpoListArgs:        "standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "standardC"},
+			},
+		},
+		"Standard policy, computer object": {
+			gpoListArgs:        "standard",
+			objectName:         hostname,
+			objectClass:        ad.ComputerObject,
+			userKrb5CCBaseName: "", // ignored for machine
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/D": {Key: "Software/Ubuntu/D", Value: "standardD"},
+				"Software/Ubuntu/E": {Key: "Software/Ubuntu/E", Value: "standardE"},
+			},
+		},
+		"User only policy, user object": {
+			gpoListArgs:        "user-only",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "userOnlyA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "userOnlyB"},
+			},
+		},
+		"User only policy, computer object": {
+			gpoListArgs:        "user-only",
+			objectName:         hostname,
+			objectClass:        ad.ComputerObject,
+			userKrb5CCBaseName: "",
+			want:               map[string]policy.Entry{},
+		},
+		"Computer only policy, user object": {
+			gpoListArgs:        "machine-only",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want:               map[string]policy.Entry{},
+		},
+		"Computer ignored CCBaseName": {
+			gpoListArgs:        "standard",
+			objectName:         hostname,
+			objectClass:        ad.ComputerObject,
+			userKrb5CCBaseName: "somethingtotallyarbitrary", // ignored for machine
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/D": {Key: "Software/Ubuntu/D", Value: "standardD"},
+				"Software/Ubuntu/E": {Key: "Software/Ubuntu/E", Value: "standardE"},
+			},
+		},
+
+		"Two policies, with overrides": {
+			gpoListArgs:        "one-value_standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "oneValueC"},
+			},
+		},
+		"Two policies, with reversed overrides": {
+			gpoListArgs:        "standard_one-value",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "standardC"},
+			},
+		},
+		"Two policies, no overrides": {
+			gpoListArgs:        "one-value_user-only",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "userOnlyA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "userOnlyB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "oneValueC"},
+			},
+		},
+		"Two policies, no overrides, reversed": {
+			gpoListArgs:        "user-only_one-value",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "userOnlyA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "userOnlyB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "oneValueC"},
+			},
+		},
+		"Two policies, no overrides, one is not the same object type": {
+			gpoListArgs:        "machine-only_standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "standardC"},
+			},
+		},
+
+		"Disabled value overrides non disabled one": {
+			gpoListArgs:        "disabled-value_standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "", Disabled: true},
+			},
+		},
+		"Disabled value is overridden": {
+			gpoListArgs:        "standard_disabled-value",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "standardC"},
+			},
+		},
+
+		"More policies, with multiple overrides": {
+			gpoListArgs:        "user-only_one-value_standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			want: map[string]policy.Entry{
+				"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "userOnlyA"},
+				"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "userOnlyB"},
+				"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "oneValueC"},
+			},
+		},
+
+		// Error cases
+		"Machine doesn’t match": {
+			gpoListArgs:        "standard",
+			objectName:         "NotHostname",
+			objectClass:        ad.ComputerObject,
+			userKrb5CCBaseName: "",
+			wantErr:            true,
+		},
+		"Without previous call, needs userKrb5CCBaseName": {
+			gpoListArgs:        "standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "",
+			wantErr:            true,
+		},
+		"Unexisting CC original file for user": {
+			gpoListArgs:                  "standard",
+			objectName:                   "bob",
+			objectClass:                  ad.UserObject,
+			userKrb5CCBaseName:           "kbr5cc_adsys_tests_bob",
+			dontCreateOriginalKrb5CCName: true,
+			wantErr:                      true,
+		},
+		"Unexisting CC original file for machine": {
+			gpoListArgs:                  "standard",
+			objectName:                   hostname,
+			objectClass:                  ad.ComputerObject,
+			userKrb5CCBaseName:           "",
+			dontCreateOriginalKrb5CCName: true,
+			wantErr:                      true,
+		},
+		"Corrupted policy file": {
+			gpoListArgs:        "corrupted-policy",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			wantErr:            true,
+		},
+		"Policy can’t be downloaded": {
+			gpoListArgs:        "no-gpt-ini",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			wantErr:            true,
+		},
+		"Symlinks can’t be created": {
+			gpoListArgs:        "standard",
+			objectName:         "bob",
+			objectClass:        ad.UserObject,
+			userKrb5CCBaseName: "kbr5cc_adsys_tests_bob",
+			turnKrb5CCRO:       true,
+			wantErr:            true,
+		},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			//t.Parallel() // libsmbclient overrides SIGCHILD, keep one AD object
+
+			// only create original cc file when requested
+			krb5CCName := tc.userKrb5CCBaseName
+			if krb5CCName != "" && !tc.dontCreateOriginalKrb5CCName {
+				f, err := ioutil.TempFile("", tc.userKrb5CCBaseName+"_*")
+				require.NoError(t, err, "Setup: failed to create temporary krb5 cache file")
+				defer f.Close()
+				krb5CCName = f.Name()
+				defer os.Remove(krb5CCName) // clean up
+				_, err = f.Write(krb5CCcontent)
+				require.NoError(t, err, "Setup: failed to write to temporary krb5 cache file")
+				require.NoError(t, f.Close(), "Setup: failed to close temporary krb5 cache file")
+			}
+
+			cachedir, rundir := t.TempDir(), t.TempDir()
+			hostKrb5CCName := filepath.Join(rundir, "krb5cc", hostname)
+			if tc.dontCreateOriginalKrb5CCName {
+				hostKrb5CCName = ""
+			}
+			adc, err := ad.New(context.Background(), "ldap://UNUSED:1636/", "warthogs.biz",
+				ad.WithCacheDir(cachedir), ad.WithRunDir(rundir), ad.WithoutKerberos(),
+				ad.WithKinitCmd(ad.MockKinitWithComputer{hostKrb5CCName}),
+				ad.WithGPOListCmd(mockGPOListCmd(t, tc.gpoListArgs)))
+			require.NoError(t, err, "Setup: cannot create ad object")
+
+			if tc.turnKrb5CCRO {
+				os.Chmod(adc.Krb5CacheDir(), 0400)
+				defer func() {
+					os.Chmod(adc.Krb5CacheDir(), 0700)
+				}()
+			}
+
+			entries, err := adc.GetPolicies(context.Background(), tc.objectName, tc.objectClass, krb5CCName)
+			if tc.wantErr {
+				require.NotNil(t, err, "GetPolicies should have errored out")
+			} else {
+				require.NoError(t, err, "GetPolicies should return no error")
+			}
+			require.Equal(t, tc.want, entries, "GetPolicies returns expected policy entries with correct overrides")
+		})
+	}
+}
+
+func TestGetPoliciesForRefresh(t *testing.T) {
+	//t.Parallel() // libsmbclient overrides SIGCHILD, keep one AD object
+
+	gpoListArgs := "standard"
+	objectName := "bob"
+	objectClass := ad.UserObject
+	want := map[string]policy.Entry{
+		"Software/Ubuntu/A": {Key: "Software/Ubuntu/A", Value: "standardA"},
+		"Software/Ubuntu/B": {Key: "Software/Ubuntu/B", Value: "standardB"},
+		"Software/Ubuntu/C": {Key: "Software/Ubuntu/C", Value: "standardC"},
+	}
+
+	var krb5CCcontent = []byte("KRB5 Ticket file content")
+	f, err := ioutil.TempFile("", "kbr5cc_adsys_tests_bob_*")
+	require.NoError(t, err, "Setup: failed to create temporary krb5 cache file")
+	defer f.Close()
+	krb5CCName := f.Name()
+	defer os.Remove(krb5CCName) // clean up
+	_, err = f.Write(krb5CCcontent)
+	require.NoError(t, err, "Setup: failed to write to temporary krb5 cache file")
+	require.NoError(t, f.Close(), "Setup: failed to close temporary krb5 cache file")
+
+	cachedir, rundir := t.TempDir(), t.TempDir()
+
+	adc, err := ad.New(context.Background(), "ldap://UNUSED:1636/", "warthogs.biz",
+		ad.WithCacheDir(cachedir), ad.WithRunDir(rundir), ad.WithoutKerberos(),
+		ad.WithKinitCmd(ad.MockKinit{}),
+		ad.WithGPOListCmd(mockGPOListCmd(t, gpoListArgs)))
+	require.NoError(t, err, "Setup: cannot create ad object")
+
+	// First call create symlink
+	entries, err := adc.GetPolicies(context.Background(), objectName, objectClass, krb5CCName)
+	require.NoError(t, err, "GetPolicies should return no error")
+	require.Equal(t, want, entries, "GetPolicies returns expected policy entries with correct overrides")
+
+	// Prepare gpo list to be reexecuted
+	adc.ResetGpoListCmdWith(mockGPOListCmd(t, gpoListArgs))
+
+	// Second call withut CCName should use symlink (refresh case)
+	entries, err = adc.GetPolicies(context.Background(), objectName, objectClass, "")
+	require.NoError(t, err, "GetPolicies should return no error")
+	require.Equal(t, want, entries, "GetPolicies returns expected policy entries with correct overrides")
+}
+
+func TestMockGPOList(t *testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+	defer os.Exit(0)
+
+	krb5File := os.Getenv("KRB5CCNAME")
+	if _, err := os.Lstat(krb5File); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Expecting symlink %s to exists", krb5File)
+		os.Exit(1)
+	}
+	if _, err := os.Stat(krb5File); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Expecting file pointed by %s to exists", krb5File)
+		os.Exit(1)
+	}
+
+	args := os.Args
+	for len(args) > 0 {
+		if args[0] != "--" {
+			args = args[1:]
+			continue
+		}
+		args = args[1:]
+		break
+	}
+
+	gpos := strings.Split(args[0], "_")
+	for _, gpo := range gpos {
+		fmt.Fprintf(os.Stdout, "%s\tsmb://localhost:%d/SYSVOL/warthogs.biz/Policies/%s\n", gpo, ad.SmbPort, gpo)
+	}
+}
+
+func mockGPOListCmd(t *testing.T, args ...string) *exec.Cmd {
+	t.Helper()
+
+	cArgs := []string{"-test.run=TestMockGPOList", "--"}
+	cArgs = append(cArgs, args...)
+
+	cmd := exec.Command(os.Args[0], cArgs...)
+	cmd.Env = append(os.Environ(), "GO_WANT_HELPER_PROCESS=1")
+	return cmd
 }
--- internal/policies/ad/export_test.go
+++ internal/policies/ad/export_test.go
@@ -2,6 +2,8 @@ package ad
 
 import (
 	"errors"
+	"os"
+	"os/exec"
 )
 
 var (
@@ -19,6 +21,10 @@ func (ad *AD) Krb5CacheDir() string {
 	return ad.krb5CacheDir
 }
 
+func (ad *AD) ResetGpoListCmdWith(cmd *exec.Cmd) {
+	ad.gpoListCmd = cmd
+}
+
 type MockKinit struct {
 	Failing bool
 }
@@ -26,6 +32,27 @@ type MockKinit struct {
 func (m MockKinit) CombinedOutput() ([]byte, error) {
 	if m.Failing {
 		return []byte("My error message on stderr"), errors.New("Exit 1")
+	}
+	return nil, nil
+}
+
+type MockKinitWithComputer struct {
+	HostKrb5CCName string
+}
+
+func (m MockKinitWithComputer) CombinedOutput() ([]byte, error) {
+	if m.HostKrb5CCName != "" {
+		f, err := os.Create(m.HostKrb5CCName)
+		if err != nil {
+			return []byte("Setup: failed to open host krb5 cache file"), err
+		}
+		if _, err = f.Write([]byte("krb5CCcontent")); err != nil {
+			return []byte("Setup: failed to write to host krb5 cache file"), err
+		}
+
+		if err = f.Close(); err != nil {
+			return []byte("Setup: failed to close host krb5 cache file"), err
+		}
 	}
 	return nil, nil
 }
