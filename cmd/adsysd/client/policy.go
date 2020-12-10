2c7c365371b002f64d316b210cc5131794b3fa51
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2020-12-10 12:24:20 +0100

    Pass computer name and file path only as ticket
    Strips FILE: prefix from ticket and ensures the client always pass the
    computer name.
    
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

--- cmd/adsysd/client/policy.go
+++ cmd/adsysd/client/policy.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/user"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/ubuntu/adsys"
@@ -24,7 +25,7 @@ func (a *App) installPolicy() {
 
 	var updateMachine *bool
 	cmd := &cobra.Command{
-		Use:   "update [USER KERBEROS_TICKET_PATH]",
+		Use:   "update [USER_NAME KERBEROS_TICKET_PATH]",
 		Short: i18n.G("Updates/Create a policy for current user or given user with its kerberos ticket"),
 		Args:  cmdhandler.ZeroOrNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -39,24 +40,36 @@ func (a *App) installPolicy() {
 	mainCmd.AddCommand(cmd)
 }
 
-func (a *App) policyUpdate(isComputer bool, targetedUser, krb5cc string) error {
+func (a *App) policyUpdate(isComputer bool, target, krb5cc string) error {
 	client, err := adsysservice.NewClient(a.config.Socket, a.getTimeout())
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 
+	// override for computer
+	if isComputer && target == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			return err
+		}
+		// for malconfigured machines where /proc/sys/kernel/hostname returns the fqdn and not only the machine name, strip it
+		if i := strings.Index(hostname, "."); i > 0 {
+			target = hostname[:i]
+		}
+	}
+
 	// Update for current user
-	if targetedUser == "" {
+	if target == "" {
 		u, err := user.Current()
 		if err != nil {
 			return fmt.Errorf("failed to retrieve current user: %v", err)
 		}
-		targetedUser = u.Username
-		krb5cc = os.Getenv("KRB5CCNAME")
+		target = u.Username
+		krb5cc = strings.TrimPrefix(os.Getenv("KRB5CCNAME"), "FILE:")
 	}
 
-	stream, err := client.UpdatePolicy(a.ctx, &adsys.UpdatePolicyRequest{IsComputer: isComputer, User: targetedUser, Krb5Cc: krb5cc})
+	stream, err := client.UpdatePolicy(a.ctx, &adsys.UpdatePolicyRequest{IsComputer: isComputer, User: target, Krb5Cc: krb5cc})
 	if err != nil {
 		return err
 	}
