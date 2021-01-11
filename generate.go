f86cbe5ac79695de1a2df834a964bd52a4eb2312
Didier Roche <didrocks@ubuntu.com>
2021-01-11 10:19:11 +0100

    Support install mode in generator
    Ensure we check for install mode only in generators.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package adsys
 
-//go:generate sh -c "PATH=\"$PATH:`go env GOPATH`/bin\" protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative adsys.proto"
+//go:generate sh -c "if go run internal/generators/can_modify_repo.go 2>/dev/null; then PATH=\"$PATH:`go env GOPATH`/bin\" protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative adsys.proto; fi"
--- internal/grpc/logstreamer/common.go
+++ internal/grpc/logstreamer/common.go
@@ -1,6 +1,6 @@
 package log
 
-//go:generate sh -c "PATH=\"$PATH:`go env GOPATH`/bin\" protoc --go_out=. --go_opt=paths=source_relative log.proto"
+//go:generate sh -c "if go run internal/generators/can_modify_repo.go 2>/dev/null; then PATH=\"$PATH:`go env GOPATH`/bin\" protoc --go_out=. --go_opt=paths=source_relative log.proto; fi"
 
 const (
 	logIdentifier = "LOGSTREAMER_MSG"
