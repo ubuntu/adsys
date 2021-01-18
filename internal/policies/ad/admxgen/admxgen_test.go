08daae874c7ffc1627a3e57d84d58eab9c23a5ff
Didier Roche <didrocks@ubuntu.com>
2021-01-18 11:15:44 +0100

    Test expand command in generator
    We only test expand for now with dconf. We will add later more use
    cases once we support other generators.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/policies/ad/admxgen/admxgen_test.go
+++ internal/policies/ad/admxgen/admxgen_test.go
@@ -6,10 +6,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/ubuntu/adsys/internal/policies/ad/admxgen/common"
 	"gopkg.in/yaml.v2"
 )
 
@@ -154,6 +156,71 @@ func TestExpandedCategoriesToADMX(t *testing.T) {
 
 			assert.Equal(t, string(wantADMX), string(gotADMX), "expected and got admx content differs")
 			assert.Equal(t, string(wantADML), string(gotADML), "expected and got adml content differs")
+		})
+	}
+}
+
+func TestMainExpand(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		root string
+
+		wantErr bool
+	}{
+		"dconf": {root: "simple"},
+
+		/* Error cases */
+		"no release file":         {root: "no release file", wantErr: true},
+		"no version_id":           {root: "no version id", wantErr: true},
+		"unsupported policy type": {root: "simple", wantErr: true},
+		"no source directory":     {root: "simple", wantErr: true},
+		"invalid dconf.yaml":      {root: "simple", wantErr: true},
+		"dconf generation fails":  {root: "unsupported dconf type", wantErr: true},
+	}
+	for name, tc := range tests {
+		name := name
+
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			src := filepath.Join("testdata", "expand", "defs", name)
+			dst := t.TempDir()
+			root := filepath.Join("testdata", "expand", "system", tc.root)
+
+			currentSession := "ubuntu"
+			err := expand(src, dst, root, currentSession)
+			if tc.wantErr {
+				require.Error(t, err, "expand should have errored out")
+				return
+			}
+			require.NoError(t, err, "expand failed but shouldn't have")
+
+			data, err := ioutil.ReadFile(filepath.Join(dst, "20.04.yaml"))
+			require.NoError(t, err, "failed to generate expanded policies file")
+			goldPath := filepath.Join("testdata", "expand", "golden", name)
+			var got, want []common.ExpandedPolicy
+			err = yaml.Unmarshal(data, &got)
+
+			// Order the policies (as we collect them as soon as the generator returns)
+			// Finale admx is not impacted as we use categories definition to order policies
+			expandedPoliciesByType := make(map[string][]common.ExpandedPolicy)
+			var types []string
+			for _, p := range got {
+				types = append(types, p.Type)
+				expandedPoliciesByType[p.Type] = append(expandedPoliciesByType[p.Type], p)
+			}
+			sort.Strings(types)
+			var gotPolicies []common.ExpandedPolicy
+			for _, t := range types {
+				gotPolicies = append(gotPolicies, expandedPoliciesByType[t]...)
+			}
+
+			require.NoError(t, err, "created file is not a slice of expanded policy objects")
+			wantFromGoldenFileYAML(t, goldPath, gotPolicies, &want)
+
+			assert.Equal(t, want, gotPolicies, "expected and got differs")
 		})
 	}
 }
