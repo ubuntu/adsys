d190d211a2ed5b4350a4d720b0c9b0a768d04b0e
Didier Roche <didrocks@ubuntu.com>
2021-04-09 11:00:38 +0200

    Service Cat integration test
    As we may have mixed daemon/clients or clients/clients data race which
    won’t happen in a real setup, run some parts as separate subprocesses
    while testing the rest.
    
    Thus, we have 2 tests layout for each test case:
    - One covering daemon side code, with daemon in process, and cat +
      version in their own subprocess. We check the cat output.
    - One covering client side code, with cat in process, and daemon +
      version in their own subprocess. We check the cat logger output.

--- cmd/adsysd/integration_tests/adsysctl_service_test.go
+++ cmd/adsysd/integration_tests/adsysctl_service_test.go
@@ -1,10 +1,17 @@
 package adsys_test
 
 import (
+	"bytes"
+	"errors"
+	"io"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/ubuntu/adsys/cmd/adsysd/client"
 )
 
 func TestStop(t *testing.T) {
@@ -40,6 +47,109 @@ func TestStop(t *testing.T) {
 				return
 			}
 			require.NoError(t, err, "client should exit with no error")
+		})
+	}
+}
+
+func TestCat(t *testing.T) {
+	// Unfortunately, we can’t easily create the cat client and other pingers in the same process:
+	// as cat will print what was forwarded to it, and the daemon, other clients and such will all write
+	// also, this creates multiple calls, with overriding fds and such.
+
+	// Keep coverage by either switching the daemon or the cat client in their own process.
+	// Always keep version in its own process.
+
+	tests := map[string]struct {
+		polkitAnswer     string
+		daemonNotStarted bool
+		coverCatClient   bool
+
+		wantErr bool
+	}{
+		"Cat other clients and daemon - cover daemon": {polkitAnswer: "yes"},
+		"Cat denied - cover daemon":                   {polkitAnswer: "no", wantErr: true},
+		"Daemon not responding - cover daemon":        {daemonNotStarted: true, wantErr: true},
+
+		"Cat other clients and daemon - cover client": {polkitAnswer: "yes", coverCatClient: true},
+		"Cat denied - cover client":                   {polkitAnswer: "no", coverCatClient: true, wantErr: true},
+		"Daemon not responding - cover client":        {daemonNotStarted: true, coverCatClient: true, wantErr: true},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			defer polkitAnswer(t, tc.polkitAnswer)()
+
+			startInProcessDaemon := !tc.daemonNotStarted && !tc.coverCatClient
+			conf, quit := runDaemon(t, startInProcessDaemon)
+			defer quit()
+
+			if tc.coverCatClient {
+				_, stopDaemon, err := startCmd(t, false, "adsysd", "-c", conf)
+				require.NoError(t, err, "daemon should start successfully")
+				defer stopDaemon()
+				// Let the daemon starting
+				time.Sleep(5 * time.Second)
+			}
+
+			var outCat func() string
+			var stopCat func() error
+			if tc.coverCatClient {
+				// create cat client and control it, capturing stderr for logs
+
+				// capture log output (set to stderr, but captured when loading logrus)
+				r, w, err := os.Pipe()
+				require.NoError(t, err, "Setup: pipe shouldn’t fail")
+				orig := logrus.StandardLogger().Out
+				logrus.StandardLogger().SetOutput(w)
+
+				c := client.New()
+
+				var out bytes.Buffer
+				done := make(chan struct{})
+				go func() {
+					defer close(done)
+					defer changeOsArgs(t, conf, "service", "cat")()
+					err = c.Run()
+				}()
+
+				outCat = func() string {
+					return out.String()
+				}
+				stopCat = func() error {
+					c.Quit()
+					<-done
+					logrus.StandardLogger().SetOutput(orig)
+					w.Close()
+					_, errCopy := io.Copy(&out, r)
+					require.NoError(t, errCopy, "Couldn’t copy stderr to buffer")
+					return errors.New("Mimic cat killing")
+				}
+			} else {
+				var err error
+				outCat, stopCat, err = startCmd(t, false, "adsysctl", "-vv", "-c", conf, "service", "cat")
+				require.NoError(t, err, "cat should start successfully")
+			}
+
+			// Let cat connecting to daemon and daemon forwarding logs
+			time.Sleep(time.Second)
+
+			// Second client we will spy logs on
+			_, _, err := startCmd(t, true, "adsysctl", "-vv", "-c", conf, "version")
+			if !tc.wantErr {
+				require.NoError(t, err, "version should run successfully")
+			}
+
+			err = stopCat()
+			require.Error(t, err, "cat has been killed")
+
+			if tc.wantErr {
+				assert.NotContains(t, outCat(), "New connection from client", "no internal logs from server are forwarded")
+				assert.NotContains(t, outCat(), "New request /service/Version", "no debug logs for clients are forwarded")
+				return
+			}
+
+			assert.Contains(t, outCat(), "New connection from client", "internal logs from server are forwarded")
+			assert.Contains(t, outCat(), "New request /service/Version", "debug logs for clients are forwarded")
 		})
 	}
 }
