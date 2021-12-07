18dd91c9b9549b1bf50ea5725f9e357985a794ec
Didier Roche <didrocks@ubuntu.com>
2021-12-07 11:33:22 +0000

    Unit tests for getting local policykit administrators
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/policies/privilege/internal_test.go
+++ internal/policies/privilege/internal_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSplitAndNormalizeUsersAndGroups(t *testing.T) {
@@ -58,6 +59,48 @@ func TestSplitAndNormalizeUsersAndGroups(t *testing.T) {
 
 			got := splitAndNormalizeUsersAndGroups(context.Background(), tc.input)
 			assert.Equal(t, tc.want, got, "splitAndNormalizeUsersAndGroups returned expected value")
+		})
+	}
+}
+
+func TestGetSystemPolkitAdminIdentities(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		policyKitDir string
+
+		want    string
+		wantErr bool
+	}{
+		"Fetch previous admin identities": {policyKitDir: "testdata/existing-previous-local-admins-one/polkit-1",
+			want: "unix-user:local50admin1;unix-user:local50admin2"},
+		"Fetch previous admin identities from highest ascii file": {policyKitDir: "testdata/existing-previous-local-admins-multi/polkit-1",
+			want: "unix-user:local50admin1;unix-user:local50admin2"},
+		"Fetch previous admin identities ignoring adsys": {policyKitDir: "testdata/existing-previous-local-admins-with-adsys-file/polkit-1",
+			want: "unix-user:local50admin1;unix-user:local50admin2"},
+
+		// Edge cases
+		"No previous admin identities but regular directory structure": {policyKitDir: "testdata/existing-other-files/polkit-1",
+			want: ""},
+		"Returns an empty string if directory does not exists": {policyKitDir: "testdata/doesnotexists",
+			want: ""},
+		"Directory instead of a conf file is ignored": {policyKitDir: "testdata/incorrect-policikit-conf-is-dir/polkit-1",
+			want: ""},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := getSystemPolkitAdminIdentities(context.Background(), tc.policyKitDir)
+			if tc.wantErr {
+				require.NotNil(t, err, "getSystemPolkitAdminIdentities should have failed but didn't")
+				return
+			}
+			require.NoError(t, err, "ApplyPolicy failed but shouldn't have")
+
+			assert.Equal(t, tc.want, got, "getSystemPolkitAdminIdentities returned expected value")
 		})
 	}
 }
