33e0c9cfd0c71f1e8b2702a1a72619ed141d3f66
Gabriel Nagy <gabriel.nagy@canonical.com>
2022-10-13 10:44:21 +0300

    Implement partial removal and caching for apparmor
    To handle the one profile -> many policies issue we make use of the
    unprivileged `apparmor_parser -N` command which parses the input files
    and returns a list of policy names which we can then pipe to
    `apparmor_parser -R` for removal.
    
    Profiles and policies under /etc/apparmor.d/adsys are cross-referenced
    with the contents of /sys/kernel/security/apparmor/profiles to determine
    which policies are actually loaded in the system. This is to ensure we
    don't attempt to unload a policy under /etc/apparmor.d/adsys that
    happens to not be loaded in the system for any reason.
    
    Caching is built in into apparmor_parser, so we can leverage its
    functionality. Adding -W when loading profiles will first attempt to
    resolve profiles from cache if present, then write the updated cache to
    /var/cache/adsys/apparmor.
    
    DEENG-461 / DEENG-476

--- internal/policies/apparmor/apparmor_test.go
+++ internal/policies/apparmor/apparmor_test.go
@@ -1,12 +1,15 @@
 package apparmor_test
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"io/fs"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"testing"
 
@@ -15,6 +18,7 @@ import (
 	"github.com/ubuntu/adsys/internal/policies/apparmor"
 	"github.com/ubuntu/adsys/internal/policies/entry"
 	"github.com/ubuntu/adsys/internal/testutils"
+	"golang.org/x/exp/slices"
 )
 
 var update bool
@@ -28,52 +32,60 @@ func TestApplyPolicy(t *testing.T) {
 		entries []entry.Entry
 		user    bool
 
-		noParserOutput          bool
+		noParserOutput         bool
+		destAlreadyExists      string
+		readOnlyApparmorDir    string
+		noApparmorParser       bool
+		existingLoadedPolicies []string
+
 		saveAssetsError         bool
 		removeUnusedAssetsError bool
-		apparmorParserError     bool
-		destAlreadyExists       string
-		readOnlyApparmorDir     string
-		noApparmorParser        bool
+		apparmorParserError     string
 
 		wantErr bool
 	}{
 		// computer cases
-		"computer, one profile":              {entries: defaultMachineProfile},
-		"computer, multiple profiles,":       {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\nusr.bin.bar\nnested/usr.bin.baz"}}},
-		"computer, duplicated profiles":      {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\nusr.bin.foo"}}},
-		"computer, blank line profiles":      {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\n\nusr.bin.bar\n"}}},
-		"computer, profiles with whitespace": {entries: []entry.Entry{{Key: "apparmor-machine", Value: " usr.bin.foo\n\n usr.bin.bar   \nnested/usr.bin.baz "}}},
-		"computer, whitespace-only value":    {entries: []entry.Entry{{Key: "apparmor-machine", Value: "       "}}, noParserOutput: true},
-		"computer, only blank profiles":      {entries: []entry.Entry{{Key: "apparmor-machine", Value: "\n\n\n"}}, noParserOutput: true},
-		"existing .old directory is removed": {entries: defaultMachineProfile, destAlreadyExists: "machine.old", noParserOutput: true},
-		"existing .new directory is removed": {entries: defaultMachineProfile, destAlreadyExists: "machine.new", noParserOutput: true},
+		"computer, one profile":                    {},
+		"computer, multiple profiles,":             {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\nusr.bin.bar\nnested/usr.bin.baz"}}},
+		"computer, duplicated profiles":            {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\nusr.bin.foo"}}},
+		"computer, blank line profiles":            {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo\n\nusr.bin.bar\n"}}},
+		"computer, profiles with whitespace":       {entries: []entry.Entry{{Key: "apparmor-machine", Value: " usr.bin.foo\n\n usr.bin.bar   \nnested/usr.bin.baz "}}},
+		"computer, whitespace-only value":          {entries: []entry.Entry{{Key: "apparmor-machine", Value: "       "}}, noParserOutput: true},
+		"computer, only blank profiles":            {entries: []entry.Entry{{Key: "apparmor-machine", Value: "\n\n\n"}}, noParserOutput: true},
+		"computer, previous profiles are unloaded": {destAlreadyExists: "machine", existingLoadedPolicies: []string{"/usr/bin/foo", "/usr/bin/bar", "/usr/bin/baz"}},
+		"existing .old directory is removed":       {destAlreadyExists: "machine.old", noParserOutput: true},
+		"existing .new directory is removed":       {destAlreadyExists: "machine.new", noParserOutput: true},
 
 		// shared cases
-		"no profiles, existing rules are removed": {destAlreadyExists: "machine"},
-		"no profiles, apparmor directory absent":  {noParserOutput: true},
+		"no profiles, existing rules are removed": {entries: []entry.Entry{}, destAlreadyExists: "machine", existingLoadedPolicies: []string{"/usr/bin/foo", "/usr/bin/bar", "/usr/bin/baz"}},
+		"no profiles, apparmor directory absent":  {entries: []entry.Entry{}, noParserOutput: true},
 		"unexpected entry key":                    {entries: []entry.Entry{{Key: "apparmor-foo", Value: "usr.bin.foo"}}, noParserOutput: true},
 
 		// user cases
 		"user, one profile": {entries: []entry.Entry{{Key: "apparmor-user", Value: "usr.bin.foo"}}, user: true, noParserOutput: true},
 
 		// other edge cases
-		"no apparmor_parser and no entries": {noApparmorParser: true, noParserOutput: true},
-		"no apparmor_parser and entries":    {entries: defaultMachineProfile, noApparmorParser: true, noParserOutput: true, wantErr: true},
+		"no apparmor_parser and no entries": {entries: []entry.Entry{}, noApparmorParser: true, noParserOutput: true},
+		"no apparmor_parser and entries":    {noApparmorParser: true, noParserOutput: true, wantErr: true},
 
 		// error cases
-		"error on parsing profiles failing":              {entries: defaultMachineProfile, apparmorParserError: true, wantErr: true},
-		"error on unloading profiles failing":            {destAlreadyExists: "machine", apparmorParserError: true, wantErr: true},
-		"error on save assets dumping failing":           {entries: defaultMachineProfile, noParserOutput: true, saveAssetsError: true, wantErr: true},
-		"error on removing unused assets after dump":     {entries: defaultMachineProfile, noParserOutput: true, removeUnusedAssetsError: true, wantErr: true},
-		"error on profile being a directory":             {entries: []entry.Entry{{Key: "apparmor-machine", Value: "nested/"}}, noParserOutput: true, wantErr: true},
-		"error on absent profile":                        {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.nonexistent"}}, noParserOutput: true, wantErr: true},
-		"error on file as a directory":                   {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo/notadir"}}, noParserOutput: true, wantErr: true},
-		"error on read-only root directory, no entries":  {readOnlyApparmorDir: ".", noParserOutput: true, wantErr: true},
-		"error on read-only root directory with entries": {entries: defaultMachineProfile, readOnlyApparmorDir: ".", noParserOutput: true, wantErr: true},
-		"error on read-only machine directory":           {entries: defaultMachineProfile, destAlreadyExists: "machine", readOnlyApparmorDir: "machine", noParserOutput: true, wantErr: true},
-		"error on read-only .old directory":              {entries: defaultMachineProfile, destAlreadyExists: "machine.old", readOnlyApparmorDir: "machine.old", noParserOutput: true, wantErr: true},
-		"error on read-only .new directory":              {entries: defaultMachineProfile, destAlreadyExists: "machine.new", readOnlyApparmorDir: "machine.new", noParserOutput: true, wantErr: true},
+		"error on loading profiles failing":                {apparmorParserError: "-r", wantErr: true},
+		"error on preprocessing new profiles failing":      {apparmorParserError: "-N", wantErr: true},
+		"error on preprocessing old profiles failing":      {destAlreadyExists: "machine", existingLoadedPolicies: []string{"/usr/bin/foo"}, apparmorParserError: "-N", wantErr: true},
+		"error on unloading all profiles failing":          {entries: []entry.Entry{}, destAlreadyExists: "machine", existingLoadedPolicies: []string{"/usr/bin/foo", "/usr/bin/bar", "/usr/bin/baz"}, apparmorParserError: "-R", wantErr: true},
+		"error on unloading old profiles failing":          {destAlreadyExists: "machine", existingLoadedPolicies: []string{"/usr/bin/bar", "/usr/bin/baz"}, apparmorParserError: "-R", wantErr: true},
+		"error on save assets dumping failing":             {noParserOutput: true, saveAssetsError: true, wantErr: true},
+		"error on removing unused assets after dump":       {noParserOutput: true, removeUnusedAssetsError: true, wantErr: true},
+		"error on profile being a directory":               {entries: []entry.Entry{{Key: "apparmor-machine", Value: "nested/"}}, noParserOutput: true, wantErr: true},
+		"error on absent profile":                          {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.nonexistent"}}, noParserOutput: true, wantErr: true},
+		"error on absent loaded policies file":             {entries: []entry.Entry{}, destAlreadyExists: "machine", existingLoadedPolicies: []string{"parseError"}, noParserOutput: true, wantErr: true},
+		"error on file as a directory":                     {entries: []entry.Entry{{Key: "apparmor-machine", Value: "usr.bin.foo/notadir"}}, noParserOutput: true, wantErr: true},
+		"error on read-only root directory, no entries":    {entries: []entry.Entry{}, readOnlyApparmorDir: ".", noParserOutput: true, wantErr: true},
+		"error on read-only root directory with entries":   {readOnlyApparmorDir: ".", noParserOutput: true, wantErr: true},
+		"error on read-only machine directory":             {destAlreadyExists: "machine", readOnlyApparmorDir: "machine", noParserOutput: true, wantErr: true},
+		"error on read-only machine directory, no entries": {entries: []entry.Entry{}, destAlreadyExists: "machine", readOnlyApparmorDir: "machine/nested", noParserOutput: true, wantErr: true},
+		"error on read-only .old directory":                {destAlreadyExists: "machine.old", readOnlyApparmorDir: "machine.old", noParserOutput: true, wantErr: true},
+		"error on read-only .new directory":                {destAlreadyExists: "machine.new", readOnlyApparmorDir: "machine.new", noParserOutput: true, wantErr: true},
 	}
 
 	for name, tc := range tests {
@@ -82,14 +94,23 @@ func TestApplyPolicy(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
+			if tc.entries == nil {
+				tc.entries = defaultMachineProfile
+			}
+
 			apparmorDir := t.TempDir()
 			parserCmdOutputFile := filepath.Join(t.TempDir(), "parser-output")
+			loadedPoliciesFile := mockLoadedPoliciesFile(t, tc.existingLoadedPolicies)
+			if slices.Contains(tc.existingLoadedPolicies, "parseError") {
+				loadedPoliciesFile = "not-a-file"
+			}
 			apparmorParserCmd := mockApparmorParserCmd(t, parserCmdOutputFile)
 			if tc.noApparmorParser {
 				apparmorParserCmd = []string{"this-definitely-does-not-exist"}
 			}
-			if tc.apparmorParserError {
-				apparmorParserCmd = append(apparmorParserCmd, "-Exit1-")
+			if tc.apparmorParserError != "" {
+				// Let the mock know we want an error on a specific argument
+				apparmorParserCmd = append(apparmorParserCmd, fmt.Sprintf("-Exit1%s", tc.apparmorParserError))
 			}
 
 			object := "machine"
@@ -104,14 +125,16 @@ func TestApplyPolicy(t *testing.T) {
 						filepath.Join("testdata", "apparmor_dir", object), filepath.Join(apparmorDir, tc.destAlreadyExists),
 						&shutil.CopyTreeOptions{Symlinks: true, CopyFunction: shutil.Copy}),
 					"Setup: can't create initial apparmor dir profiles content")
-				fmt.Println(filepath.Join(apparmorDir, tc.destAlreadyExists))
 			}
 			if tc.readOnlyApparmorDir != "" {
 				testutils.MakeReadOnly(t, filepath.Join(apparmorDir, tc.readOnlyApparmorDir))
 			}
 			mockAssetsDumper := testutils.MockAssetsDumper{Err: tc.saveAssetsError, ReadOnlyErr: tc.removeUnusedAssetsError, Path: "apparmor/", T: t}
 
-			m := apparmor.New(apparmorDir, apparmor.WithApparmorParserCmd(apparmorParserCmd))
+			m := apparmor.New(apparmorDir,
+				apparmor.WithApparmorParserCmd(apparmorParserCmd),
+				apparmor.WithLoadedPoliciesFile(loadedPoliciesFile),
+			)
 			err := m.ApplyPolicy(context.Background(), "ubuntu", !tc.user, tc.entries, mockAssetsDumper.SaveAssetsTo)
 			if tc.wantErr {
 				// We don't return here as we want to check that the apparmor
@@ -192,7 +215,11 @@ func TestMockApparmorParser(t *testing.T) {
 	}
 	defer os.Exit(0)
 
+	var callParser bool
 	var outputFile string
+	var unloadedPolicies []string
+	var wantExit string
+
 	args := os.Args
 	for len(args) > 0 {
 		if args[0] != "--" {
@@ -201,16 +228,64 @@ func TestMockApparmorParser(t *testing.T) {
 		}
 		// First arg after -- is the output file to write to
 		outputFile = args[1]
+		// Args are shifted by 1 if exit was requested
+		// Remove the actual -Exit- argument in case we want to execute the
+		// underlying command
+		if strings.HasPrefix(args[2], "-Exit1-") {
+			wantExit = args[2]
+			args = args[3:]
+			break
+		}
 		args = args[2:]
 		break
 	}
+
+	// Handle specific apparmor_parser flags
+	switch args[0] {
+	case "-N":
+		// -N is an unprivileged call to apparmor_parser, so it's safe to
+		// call the command ourselves and register its output
+		callParser = true
+	case "-R":
+		// Calls to remove policies contain the policy names on stdin, which
+		// we read here and subsequently append to the parser file
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			unloadedPolicies = append(unloadedPolicies, scanner.Text())
+		}
+		require.NoError(t, scanner.Err(), "Setup: Can't read from stdin")
+	}
+
 	// Dump the newline-separated args to the output file, appending if needed
 	// in order to track multiple apparmor_parser invocations
+	if wantExit != "" {
+		appendToFile(t, outputFile, []byte(wantExit+"\n"))
+	}
 	appendToFile(t, outputFile, []byte(strings.Join(args, "\n")+"\n"))
 
-	if args[0] == "-Exit1-" {
-		fmt.Println("EXIT 1 requested in mock")
-		os.Exit(1)
+	// Dump any policies that were unloaded to the output file
+	if len(unloadedPolicies) > 0 {
+		// Sort the policies to make the output deterministic in parallel testing
+		sort.Strings(unloadedPolicies)
+		appendToFile(t, outputFile, []byte(strings.Join(unloadedPolicies, "\n")+"\n"))
+	}
+
+	// Call the real apparmor_parser command if taking an unprivileged action
+	if callParser {
+		// #nosec G204 - We are in control of the arguments in tests
+		cmd := exec.Command("apparmor_parser", args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		require.NoError(t, err, "Setup: Calling apparmor_parser -N failed")
+	}
+
+	if wantExit != "" {
+		// Only exit on the requested argument
+		if strings.HasSuffix(wantExit, args[0]) {
+			fmt.Println("EXIT 1 requested in mock")
+			os.Exit(1)
+		}
 	}
 }
 
@@ -225,6 +300,17 @@ func userOrMachine(user bool) string {
 		return "user"
 	}
 	return "machine"
+}
+
+func mockLoadedPoliciesFile(t *testing.T, policies []string) string {
+	t.Helper()
+
+	// The contents of this file are cross-referenced with the apparmor.d/adsys
+	// directory structure in order to determine which of the policies are loaded.
+	path := filepath.Join(t.TempDir(), "loaded-policies")
+	err := os.WriteFile(path, []byte(strings.Join(policies, " (enforce)\n")+" (enforce)\n"), 0600)
+	require.NoError(t, err, "Setup: Can't write loaded policies file")
+	return path
 }
 
 func TestMain(m *testing.M) {
