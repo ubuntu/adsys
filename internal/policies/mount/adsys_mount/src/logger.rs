63ccff0308d8822cba8fa8941ce1bcb1ae2cee87
denisonbarbosa <denison.barbosa@canonical.com>
2022-10-13 13:06:56 -0400

    Updating logger to print into stderr

--- internal/policies/mount/adsys_mount/src/logger.rs
+++ internal/policies/mount/adsys_mount/src/logger.rs
@@ -9,7 +9,7 @@ impl Log for Logger {
 
     fn log(&self, record: &log::Record) {
         if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
+            eprintln!("{} - {}", record.level(), record.args());
         }
     }
 
