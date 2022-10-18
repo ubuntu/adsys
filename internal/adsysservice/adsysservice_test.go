ae77c8442bfb060e7b0608a5adfa5d2adb1c3e5d
Didier Roche <didier.roche@canonical.com>
2022-10-18 10:33:00 +0200

    Adapt tests for new adsysservice API
    We now load the backend here, adapt the existing tests for it and
    minimize the number of parameters.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/adsysservice/adsysservice_test.go
+++ internal/adsysservice/adsysservice_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/ubuntu/adsys/internal/ad/backends/sss"
 	"github.com/ubuntu/adsys/internal/adsysservice"
 	"github.com/ubuntu/adsys/internal/authorizer"
+	"github.com/ubuntu/adsys/internal/testutils"
 )
 
 type mockAuthorizer struct {
@@ -22,34 +24,37 @@ func TestNew(t *testing.T) {
 	t.Parallel()
 
 	tests := map[string]struct {
-		url                    string
-		domain                 string
-		authorizerDoneFail     error
-		AdNewFail              bool
-		existingAdsysDirs      bool
-		readUnexistingSssdConf bool
+		sssdConf string
+
+		roDir             string
+		existingAdsysDirs bool
 
 		wantNewErr bool
 	}{
-		"New and Done succeeds as expected, first run": {url: "my-ldap-url", domain: "example.com"},
-		"Adsys directory can already exists":           {url: "my-ldap-url", domain: "example.com", existingAdsysDirs: true},
+		"New and Quit succeeds as expected, first run": {},
+		"Adsys directory can already exists":           {existingAdsysDirs: true},
 
 		// Error cases
-		"Ad New fails prevents adsysservice creation":      {url: "my-ldap-url", domain: "example.com", AdNewFail: true, existingAdsysDirs: true, wantNewErr: true},
-		"No url and domain while sssdconf does not exists": {readUnexistingSssdConf: true, wantNewErr: true},
+		"Error on failure to create run directory":       {roDir: "parentrun", wantNewErr: true},
+		"Error on failure to create cache directory":     {roDir: "parentcache", wantNewErr: true},
+		"Error on nonexistent sssd.conf":                 {sssdConf: "does_not_exist", wantNewErr: true},
+		"Error on ad.New prevents adsysservice creation": {roDir: "parentcache/cache", wantNewErr: true},
 	}
 	for name, tc := range tests {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
+			if tc.sssdConf == "" {
+				tc.sssdConf = "testdata/sssd.conf"
+			}
+
 			temp := t.TempDir()
-			adsysCacheDir := filepath.Join(temp, "cache")
-			adsysRunDir := filepath.Join(temp, "run")
+			adsysCacheDir := filepath.Join(temp, "parentcache", "cache")
+			adsysRunDir := filepath.Join(temp, "parentrun", "run")
 			dconfDir := filepath.Join(temp, "dconf")
 			sudoersDir := filepath.Join(temp, "sudoers.d")
 			policyKitDir := filepath.Join(temp, "polkit-1")
-			sssCacheDir := filepath.Join(temp, "sss")
 			apparmorDir := filepath.Join(temp, "apparmor.d", "adsys")
 			apparmorFsDir := filepath.Join(temp, "apparmorfs")
 			if tc.existingAdsysDirs {
@@ -57,15 +62,15 @@ func TestNew(t *testing.T) {
 				require.NoError(t, os.MkdirAll(adsysRunDir, 0700), "Setup: could not create adsys run directory")
 			}
 
-			auth := mockAuthorizer{}
+			sssdConfig := sss.Config{
+				Conf:     tc.sssdConf,
+				CacheDir: t.TempDir(),
+			}
 
-			if tc.AdNewFail {
-				err := os.Chmod(adsysCacheDir, 0000)
-				require.NoError(t, err, "Setup: Could not prevent writing to cache directory")
-				defer func() {
-					err := os.Chmod(adsysCacheDir, 0600)
-					require.NoError(t, err, "Teardown: Could not restore writing to cache directory")
-				}()
+			if tc.roDir != "" {
+				dest := filepath.Join(temp, tc.roDir)
+				require.NoError(t, os.MkdirAll(dest, 0700), "Setup: can't create directory to make it Read Only")
+				testutils.MakeReadOnly(t, dest)
 			}
 
 			options := []adsysservice.Option{
@@ -74,19 +79,14 @@ func TestNew(t *testing.T) {
 				adsysservice.WithDconfDir(dconfDir),
 				adsysservice.WithSudoersDir(sudoersDir),
 				adsysservice.WithPolicyKitDir(policyKitDir),
-				adsysservice.WithSSSCacheDir(sssCacheDir),
 				adsysservice.WithApparmorDir(apparmorDir),
 				adsysservice.WithApparmorFsDir(apparmorFsDir),
-				adsysservice.WithMockAuthorizer(&auth),
-				adsysservice.WithDefaultDomainSuffix("mydomain.biz"),
-			}
-			if tc.readUnexistingSssdConf {
-				options = append(options, adsysservice.WithSSSdConf(filepath.Join(temp, "does-not-exists", "sssd.conf")))
+				adsysservice.WithSSSConfig(sssdConfig),
 			}
 
-			s, err := adsysservice.New(context.Background(), tc.url, tc.domain, options...)
+			s, err := adsysservice.New(context.Background(), options...)
 			if tc.wantNewErr {
-				require.Error(t, err, "New should return an error but didn’t")
+				require.Error(t, err, "New should return an error but did not")
 				return
 			}
 			require.NoError(t, err, "New should not return an error")
--- internal/adsysservice/export_test.go
+++ internal/adsysservice/export_test.go
@@ -1,20 +1,4 @@
 package adsysservice
 
-// WithMockAuthorizer specifies a personalized authorizer.
-func WithMockAuthorizer(auth authorizerer) func(o *options) error {
-	return func(o *options) error {
-		o.authorizer = auth
-		return nil
-	}
-}
-
-// WithSSSdConf specifies a personalized sssd.conf.
-func WithSSSdConf(p string) func(o *options) error {
-	return func(o *options) error {
-		o.sssdConf = p
-		return nil
-	}
-}
-
 // Option type exported for tests.
 type Option = option
