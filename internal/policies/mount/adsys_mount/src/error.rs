dbd8e5c9862a36a410b5e3e85be306f33bdd6541
denisonbarbosa <denison.barbosa@canonical.com>
2022-10-19 06:49:20 -0400

    Fixing typo in the error struct

--- internal/policies/mount/adsys_mount/src/error.rs
+++ internal/policies/mount/adsys_mount/src/error.rs
@@ -6,11 +6,11 @@ use thiserror::Error;
 /// (unless they are from the std library, i.e. io or fmt errors).
 #[non_exhaustive] // Blocks match expressions without wildcards (_) for this enum.
 #[derive(Debug, Error)]
-pub enum AdysMountError {
+pub enum AdsysMountError {
     MountError = 1,
     ParseError = 2,
 }
-impl std::fmt::Display for AdysMountError {
+impl std::fmt::Display for AdsysMountError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Process exited with error code: {:#?}", self)
     }
