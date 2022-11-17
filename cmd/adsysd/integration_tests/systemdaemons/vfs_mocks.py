ed0a98ec3d7366f3ed0f7796f420bab8e07ec6b0
denisonbarbosa <denison.barbosa@canonical.com>
2022-11-17 12:14:37 -0400

    Update VfsDbusMock to be able to force failures during the mount process

--- cmd/adsysd/integration_tests/systemdaemons/vfs_mocks.py
+++ cmd/adsysd/integration_tests/systemdaemons/vfs_mocks.py
@@ -18,7 +18,7 @@ class VfsMountTrackerMock(dbusmock.DBusMockObject):
     def MountLocation(self,
                       mount_spec: tuple[list[bytes], dict[str, list[bytes]]],
                       mount_source: tuple[str, dbus.ObjectPath]):
-        _ = self.bus.call_blocking(
+        res = self.bus.call_blocking(
             mount_source[0],
             mount_source[1],
             'org.gtk.vfs.MountOperation',
@@ -27,17 +27,35 @@ class VfsMountTrackerMock(dbusmock.DBusMockObject):
             ('message', 'ubuntu', 'WORKGROUP', 31)
         )
 
+        if res[1] == dbus.Boolean(True):
+            raise (dbus.DBusException('Authentication failed'))
+
+        protocol = ''.join([dbus.String(x) for x in mount_spec[1].get("type")]).replace('\0', '')
+        server, share = self.get_spec_values(protocol, mount_spec)
+
+        # Forces a failure if the protocol is not supported by adsys
+        if server == '' and share == '':
+            raise (dbus.DBusException('Unsupported protocol'))
+
+        # Forces a failure if the server is marked as an errored one
+        if 'error' in server:
+            raise (dbus.DBusException('Error during mount process'))
+
+        display_name = f'{share} on {server}'
+        stable_name = f'{protocol}:{server},share={share}'
+        mount_location = f'/run/user/1000/gvfs/{stable_name}'
+
         self.Mounted(
             mount_source[0],
             mount_source[1],
-            'anon_share on localhost',  # Should be dynamic
-            'smb-share:server=localhost,share=anon_share',  # Should be dynamic
+            display_name.encode(),
+            stable_name.encode(),
             '',
             '. GThemedIcon folder-remote folder folder-remote-symbolic folder-symbolic',
             '. GThemedIcon folder-remote-symbolic folder-symbolic folder-remote folder',
             '',
             True,
-            b'/run/user/1000/gvfs/smb-share:server=localhost,share=anon_share',  # Should be dynamic
+            mount_location.encode(),
             mount_spec,
             b'/'
         )
@@ -48,3 +66,20 @@ class VfsMountTrackerMock(dbusmock.DBusMockObject):
                 user_visible: bool, mount_location: list[bytes], mount_spec: tuple[list[bytes], dict[str, list[bytes]]],
                 default_location: list[bytes]):
         return
+
+    def get_spec_values(self, protocol: str, mount_spec: tuple[list[bytes], dict[str, list[bytes]]]) -> tuple[str, str]:
+        server, share = '', ''
+
+        if protocol == 'smb-share':
+            server = ''.join([dbus.String(x) for x in mount_spec[1].get("server")])
+            share = ''.join([dbus.String(x) for x in mount_spec[1].get("share")])
+
+        elif protocol == 'nfs':
+            server = ''.join([dbus.String(x) for x in mount_spec[1].get("host")])
+            share = ''.join([dbus.String(x) for x in mount_spec[0]])
+
+        # Removes the null characters from the string
+        share = share.replace('\0', '')
+        server = server.replace('\0', '')
+
+        return server, share
