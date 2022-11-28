0c2d1624889c620e815a057ffe42d339b7e661a5
denisonbarbosa <denison.barbosa@canonical.com>
2022-11-28 11:56:20 -0400

    Addressing PR comments for unsafe calls and not needed mod

--- internal/policies/mount/adsys_mount/src/unit_tests.rs
+++ internal/policies/mount/adsys_mount/src/unit_tests.rs
@@ -1,67 +1,61 @@
-#[cfg(test)]
-mod lib {
-    use crate::unit_tests::test_utils;
-    use std::collections::HashMap;
+use crate::parse_entries;
+use std::collections::HashMap;
 
-    use crate::parse_entries;
-
-    #[test]
-    fn test_parse_entries() -> Result<(), std::io::Error> {
-        struct TestCase {
-            file: &'static str,
-        }
-
-        let tests: HashMap<&str, TestCase> = HashMap::from([
-            (
-                "mounts file with one entry",
-                TestCase {
-                    file: "mounts_with_one_entry",
-                },
-            ),
-            (
-                "mounts file with multiple entries",
-                TestCase {
-                    file: "mounts_with_multiple_entries",
-                },
-            ),
-            (
-                "mounts file with anonymous entries",
-                TestCase {
-                    file: "mounts_with_anonymous_entries",
-                },
-            ),
-            (
-                "empty mounts file",
-                TestCase {
-                    file: "mounts_with_no_entry",
-                },
-            ),
-            (
-                "mounts file with bad entries",
-                TestCase {
-                    file: "mounts_with_bad_entries",
-                },
-            ),
-        ]);
-
-        for test in tests.iter() {
-            let testdata = "testdata/test_parse_entries";
-
-            let got = parse_entries(&format!("{}/{}", testdata, (test.1).file))?;
-
-            let want = test_utils::load_and_update_golden(
-                &format!("{}/{}", testdata, "golden"),
-                test.0,
-                &got,
-            )?;
-
-            assert_eq!(want, got);
-        }
-        Ok(())
+#[test]
+fn test_parse_entries() -> Result<(), std::io::Error> {
+    struct TestCase {
+        file: &'static str,
     }
+
+    let tests: HashMap<&str, TestCase> = HashMap::from([
+        (
+            "mounts file with one entry",
+            TestCase {
+                file: "mounts_with_one_entry",
+            },
+        ),
+        (
+            "mounts file with multiple entries",
+            TestCase {
+                file: "mounts_with_multiple_entries",
+            },
+        ),
+        (
+            "mounts file with anonymous entries",
+            TestCase {
+                file: "mounts_with_anonymous_entries",
+            },
+        ),
+        (
+            "empty mounts file",
+            TestCase {
+                file: "mounts_with_no_entry",
+            },
+        ),
+        (
+            "mounts file with bad entries",
+            TestCase {
+                file: "mounts_with_bad_entries",
+            },
+        ),
+    ]);
+
+    for test in tests.iter() {
+        let testdata = "testdata/test_parse_entries";
+
+        let got = parse_entries(&format!("{}/{}", testdata, (test.1).file))?;
+
+        let want = test_utils::load_and_update_golden(
+            &format!("{}/{}", testdata, "golden"),
+            test.0,
+            &got,
+        )?;
+
+        assert_eq!(want, got);
+    }
+    Ok(())
 }
 
-#[cfg(test)]
 mod test_utils {
     use serde::{Deserialize, Serialize};
     use std::{
