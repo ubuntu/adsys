cc149ed4c7216bba1d90213743552a7e97826ca7
denisonbarbosa <denison.barbosa@canonical.com>
2022-12-06 10:22:10 -0400

    Updating tests to reflect the previous commit changes

--- cmd/adsysd/integration_tests/adsys_mount_test.go
+++ cmd/adsysd/integration_tests/adsys_mount_test.go
@@ -25,18 +25,18 @@ func TestUserMountHandler(t *testing.T) {
 		wantStatus int
 	}{
 		// Single entries
-		"mount successfully nfs share": {mountsFile: "mounts_with_nfs_entry"},
-		"mount successfully smb share": {mountsFile: "mounts_with_smb_entry"},
-		"mount successfully ftp share": {mountsFile: "mounts_with_ftp_entry"},
+		"mount successfully nfs share":                     {mountsFile: "mounts_with_nfs_entry"},
+		"mount successfully smb share":                     {mountsFile: "mounts_with_smb_entry"},
+		"mount successfully ftp share":                     {mountsFile: "mounts_with_ftp_entry"},
+		"mount successfully entry without kerberos ticket": {mountsFile: "mounts_with_nfs_entry", noKrbTicket: true},
 
-		// Anonymous entries
-		"mount successfully anonymous entry":                         {mountsFile: "mounts_with_anonymous_nfs_entry"},
-		"mount successfully anonymous entry without kerberos ticket": {mountsFile: "mounts_with_anonymous_nfs_entry", noKrbTicket: true},
+		// Kerberos authentication entries
+		"mount successfully krb auth entry": {mountsFile: "mounts_with_krb_auth_entry"},
 
 		// Many entries
 		"mount successfully many entries with same protocol":       {mountsFile: "mounts_with_many_nfs_entries"},
 		"mount successfully many entries with different protocols": {mountsFile: "mounts_with_many_entries"},
-		"mount successfully many anonymous entries":                {mountsFile: "mounts_with_many_anonymous_entries"},
+		"mount successfully many kerberos auth entries":            {mountsFile: "mounts_with_many_krb_auth_entries"},
 
 		// File cases
 		"exit code 0 when file is empty": {mountsFile: "mounts_with_no_entries"},
@@ -46,8 +46,8 @@ func TestUserMountHandler(t *testing.T) {
 		"error when file doesn't exist":              {mountsFile: "do_not_exist", wantStatus: 1},
 
 		// Authentication errors
-		"error when auth is needed but no kerberos ticket is available": {mountsFile: "mounts_with_nfs_entry", noKrbTicket: true, wantStatus: 1},
-		"error when anonymous auth is not supported by the server":      {mountsFile: "mounts_with_anonymous_nfs_entry", sessionAnswer: "gvfs_anonymous_error", noKrbTicket: true, wantStatus: 1},
+		"error when auth is needed but no kerberos ticket is available": {mountsFile: "mounts_with_krb_auth_entry", noKrbTicket: true, wantStatus: 1},
+		"error when anonymous auth is not supported by the server":      {mountsFile: "mounts_with_nfs_entry", sessionAnswer: "gvfs_anonymous_error", noKrbTicket: true, wantStatus: 1},
 
 		// Bus errors
 		"error when VFS bus is not available": {mountsFile: "mounts_with_nfs_entry", sessionAnswer: "gvfs_no_vfs_bus", wantStatus: 1},
