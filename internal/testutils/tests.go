482f75333e2f78f30e6fa79664ad10d82ff9f87a
denisonbarbosa <denison.barbosa@canonical.com>
2023-03-29 09:46:57 -0400

    Adding helper function to evaluate error and stop test if requested

--- internal/testutils/tests.go
+++ internal/testutils/tests.go
@@ -3,6 +3,8 @@ package testutils
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // SkipUnlessRoot skips the test if the current user is not root.
@@ -14,4 +16,18 @@ func SkipUnlessRoot(t *testing.T) {
 	if os.Geteuid() > 0 {
 		t.Skip("Test has to be run as root, skipping...")
 	}
+}
+
+// WantError asserts the error value depending if an error is wanted and stops the test if requested.
+func WantError(t *testing.T, err error, wantErr, stopOnError bool) {
+	t.Helper()
+
+	if wantErr {
+		require.Error(t, err, "Expected %s to return an error, but it didn't", t.Name())
+		if stopOnError {
+			t.SkipNow()
+		}
+		return
+	}
+	require.NoError(t, err, "Expected %s to not return an error, but it did", t.Name())
 }
