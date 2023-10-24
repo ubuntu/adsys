63e6558a36219f80b29182d1fc6b024be84887c3
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2023-10-24 12:25:52 +0200

    Updates for new document format
    The documentation has been moved to doc/ and updated to be published to
    read the doc with in particular table of content.
    
    The documentation is now parsed to follow the ToC, point the images to
    the right addresses and remove internal links.

--- cmd/adsysd/client/doc.go
+++ cmd/adsysd/client/doc.go
@@ -1,25 +1,18 @@
 package client
 
 import (
-	"errors"
+	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/charmbracelet/glamour"
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
 	"github.com/leonelquinteros/gotext"
 	"github.com/spf13/cobra"
 	"github.com/ubuntu/adsys"
-	"github.com/ubuntu/adsys/doc"
 	"github.com/ubuntu/adsys/internal/adsysservice"
+	log "github.com/ubuntu/adsys/internal/grpc/logstreamer"
 )
 
 func (a *App) installDoc() {
-	var format, dest *string
 	docCmd := &cobra.Command{
 		Use:   "doc [CHAPTER]",
 		Short: gotext.Get("Documentation"),
@@ -33,53 +26,37 @@ func (a *App) installDoc() {
 				return nil, cobra.ShellCompDirectiveNoFileComp
 			}
 			defer client.Close()
-			stream, err := client.ListDoc(a.ctx, &adsys.ListDocRequest{Raw: true})
+			stream, err := client.ListDoc(a.ctx, &adsys.Empty{})
 			if err != nil {
 				return nil, cobra.ShellCompDirectiveNoFileComp
 			}
-			list, err := singleMsg(stream)
+			r, err := stream.Recv()
 			if err != nil {
+				log.Warningf(context.Background(), "could not receive shell completion message: %v", err)
 				return nil, cobra.ShellCompDirectiveNoFileComp
 			}
-
-			return strings.Split(list, "\n"), cobra.ShellCompDirectiveNoFileComp
+			return r.GetChapters(), cobra.ShellCompDirectiveNoFileComp
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var chapter string
 			if len(args) > 0 {
 				chapter = args[0]
 			}
-			return a.getDocumentation(chapter, *format, *dest)
+			return a.getDocumentation(chapter)
 		},
 	}
-	format = docCmd.Flags().StringP("format", "f", "markdown", gotext.Get("Format type (markdown, raw or html)."))
-	dest = docCmd.Flags().StringP("dest", "d", "", gotext.Get("Write documentation file(s) to this directory."))
 
 	a.rootCmd.AddCommand(docCmd)
 }
 
-func (a *App) getDocumentation(chapter, format, dest string) error {
-	if format != "markdown" && format != "raw" && format != "html" {
-		return fmt.Errorf("format can only be markdown, raw or html. Got %q", format)
-	}
-
+func (a *App) getDocumentation(chapter string) error {
 	client, err := adsysservice.NewClient(a.config.Socket, a.getTimeout())
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 
-	var stream recver
-	var withHeader bool
-	if dest != "" {
-		stream, err = client.GetDoc(a.ctx, &adsys.GetDocRequest{Chapter: chapter})
-		withHeader = true
-	} else if chapter == "" {
-		stream, err = client.ListDoc(a.ctx, &adsys.ListDocRequest{Raw: false})
-	} else {
-		stream, err = client.GetDoc(a.ctx, &adsys.GetDocRequest{Chapter: chapter})
-		withHeader = true
-	}
+	stream, err := client.GetDoc(a.ctx, &adsys.GetDocRequest{Chapter: chapter})
 	if err != nil {
 		return err
 	}
@@ -89,56 +66,17 @@ func (a *App) getDocumentation(chapter, format, dest string) error {
 		return err
 	}
 
-	for _, out := range strings.Split(content, doc.SplitFilesToken) {
-		if len(out) == 0 {
-			continue
-		}
-		var filename string
-		if withHeader {
-			d := strings.SplitN(out, "\n", 2)
-			filename, out = d[0], d[1]
-		}
-
-		var ext string
-		switch format {
-		case "markdown":
-			// Transform stdout content
-			if dest == "" {
-				r, err := glamour.NewTermRenderer(glamour.WithEnvironmentConfig())
-				if err != nil {
-					return err
-				}
-				out, err = r.Render(out)
-				if err != nil {
-					return err
-				}
-			}
-			ext = ".md"
-		case "html":
-			extensions := parser.CommonExtensions | parser.AutoHeadingIDs
-			parser := parser.NewWithExtensions(extensions)
-			htmlFlags := html.CommonFlags | html.HrefTargetBlank | html.TOC | html.CompletePage
-			opts := html.RendererOptions{Flags: htmlFlags}
-			renderer := html.NewRenderer(opts)
-			out = string(markdown.ToHTML([]byte(out), parser, renderer))
-			ext = ".html"
-		default:
-		}
-
-		// Write directly on stdout
-		if dest == "" {
-			fmt.Print(out)
-			continue
-		}
-
-		// Dump documentation in a directory
-		if err = os.MkdirAll(dest, 0750); err != nil {
-			return errors.New(gotext.Get("can't create %q", dest))
-		}
-		if err := os.WriteFile(filepath.Join(dest, filename+ext), []byte(out), 0600); err != nil {
-			return errors.New(gotext.Get("can't write documentation chapter %q: %v", filename+ext, err))
-		}
+	// Transform stdout content
+	r, err := glamour.NewTermRenderer(glamour.WithEnvironmentConfig())
+	if err != nil {
+		return err
 	}
+	out, err := r.Render(content)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(out)
 
 	return nil
 }
