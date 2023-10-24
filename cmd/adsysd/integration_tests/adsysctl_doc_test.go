1fab0ef1a1060c9d830ce1886d7c51ed213de17e
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2023-10-24 14:35:20 +0200

    Removed reference to doc/

--- cmd/adsysd/integration_tests/adsysctl_doc_test.go
+++ cmd/adsysd/integration_tests/adsysctl_doc_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/leonelquinteros/gotext"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/ubuntu/adsys/doc"
 )
 
 func TestDocChapter(t *testing.T) {
