2ddec9ce424a25f88219c69d515d438074923b63
Didier Roche <didier.roche@canonical.com>
2023-11-15 09:06:14 +0100

    Tests for generation policy documentation
    High level tests, similar to GenerateAD tests, to collect and bootstrap
    documentation generation.

--- internal/ad/admxgen/admxgen_test.go
+++ internal/ad/admxgen/admxgen_test.go
@@ -142,3 +142,49 @@ func TestGenerateAD(t *testing.T) {
 		})
 	}
 }
+
+func TestGenerateDoc(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		autoDetectReleases bool
+		destIsFile         bool
+
+		wantErr bool
+	}{
+		"releases from yaml":                      {},
+		"autodetect overrides releases from yaml": {autoDetectReleases: true},
+
+		// Error cases
+		"invalid definition file":  {wantErr: true},
+		"category expansion fails": {wantErr: true},
+		"doc generation fails":     {destIsFile: true, wantErr: true},
+	}
+	for name, tc := range tests {
+		name := name
+
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			catDef := filepath.Join(testutils.TestFamilyPath(t), name+".yaml")
+			src := filepath.Join(testutils.TestFamilyPath(t), "src")
+			dst := filepath.Join(t.TempDir(), "subdir")
+
+			if tc.destIsFile {
+				f, err := os.Create(dst)
+				f.Close()
+				require.NoError(t, err, "Setup: should create a file as destination")
+			}
+
+			err := admxgen.GenerateDoc(catDef, src, dst)
+			if tc.wantErr {
+				require.Error(t, err, "GenerateDoc should have errored out")
+				return
+			}
+			require.NoError(t, err, "GenerateDoc failed but shouldn't have")
+
+			testutils.CompareTreesWithFiltering(t, dst, testutils.GoldenPath(t), testutils.Update())
+		})
+	}
+}
