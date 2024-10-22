5bb965a07d7c249dc6e20a852333c865b9398a4d
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2024-10-22 11:44:54 +0200

    update go proto installation source

--- tools/tools.go
+++ tools/tools.go
@@ -5,5 +5,5 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "github.com/golang/protobuf/protoc-gen-go"
 )
