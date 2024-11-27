5cc63d94101a5fdfe2910413444bb522f00e7e25
denisonbarbosa <denison.barbosa@canonical.com>
2024-11-27 09:20:22 -0400

    Add test for new AdminIdentitiesFromRules function
    This is likely the most senstive part of this update, since we need to
    ensure we properly parse the existing files so we don't damage the
    current admin structure of the client.
    
    Therefore, it deserves a unit test

--- internal/policies/privilege/internal_test.go
+++ internal/policies/privilege/internal_test.go
@@ -102,3 +102,67 @@ func TestGetSystemPolkitAdminIdentities(t *testing.T) {
 		})
 	}
 }
+
+func TestPolkitAdminIdentitiesFromRules(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		policyKitDirs []string
+
+		emptyReturn bool
+	}{
+		"Fetch previous admin identities": {
+			policyKitDirs: []string{"existing-previous-local-admins-one/etc/polkit-1"},
+		},
+		"Fetch previous admin identities from lower ascii file": {
+			policyKitDirs: []string{"existing-previous-local-admins-multi/etc/polkit-1"},
+		},
+		"Fetch previous admin identities ignoring adsys": {
+			policyKitDirs: []string{"existing-previous-local-admins-with-adsys-file/etc/polkit-1"},
+		},
+
+		// Rules-specific cases
+		"Consider only first returned value": {
+			policyKitDirs: []string{"existing-previous-local-admins-return-early/etc/polkit-1"},
+		},
+		"Prioritize first specified directory if files have same ascii": {
+			policyKitDirs: []string{"multiple-polkit-dirs-same-file/etc/polkit-1", "multiple-polkit-dirs-same-file/etc/polkit-2"},
+		},
+		"Prioritize lower ascii file even if on second directory": {
+			policyKitDirs: []string{"multiple-polkit-dirs-diff-file/etc/polkit-1", "multiple-polkit-dirs-diff-file/etc/polkit-2"},
+		},
+
+		// Edge cases
+		"No previous admin identities but regular directory structure": {
+			policyKitDirs: []string{"existing-other-files/etc/polkit-1"},
+			emptyReturn:   true,
+		},
+		"Returns an empty string if directory does not exists": {
+			policyKitDirs: []string{"doesnotexists"},
+			emptyReturn:   true,
+		},
+		"Directory instead of a conf file is ignored": {
+			policyKitDirs: []string{"incorrect-policikit-conf-is-dir/etc/polkit-1"},
+			emptyReturn:   true,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			for i, dir := range tc.policyKitDirs {
+				tc.policyKitDirs[i] = filepath.Join("testdata", dir)
+			}
+
+			got, err := polkitAdminIdentitiesFromRules(context.Background(), tc.policyKitDirs)
+			require.NoError(t, err, "getSystemPolkitAdminIdentities failed but shouldn't have")
+
+			if tc.emptyReturn {
+				require.Empty(t, got)
+				return
+			}
+			want := testutils.LoadWithUpdateFromGolden(t, got)
+			require.Equal(t, want, got)
+		})
+	}
+}
