ea4b94928ebbb429ab9aa8900c52aea96d6a796a
shanecrowley <shane.crowley@canonical.com>
2025-02-19 11:08:02 +0000

    use name not function at top of page
    the feature is known as "ADSys".
    it's functionality as the Active Directory Group Policy client can be
    expanded in the main text.

--- docs/custom_conf.py
+++ docs/custom_conf.py
@@ -19,7 +19,7 @@ import datetime
 ############################################################
 
 # Product name
-project = "Active Directory GPO client"
+project = "ADSys"
 author = "Canonical Group Ltd"
 
 # The title you want to display for the documentation in the sidebar.
